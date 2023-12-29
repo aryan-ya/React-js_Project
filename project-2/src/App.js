67001d4c5d7a7b063ec766fbe6cbe3e2ab4ec1f1
Aryan Kumar Yadav <ay3978943@gmail.com>
2023-12-29 13:51:34 +0530

    movie

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -1,45 +1,87 @@
-
-
-import React , {useState , useEffect} from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
-import "./App.css";
-function App() {
-  const [movies , setMovies] = useState([ ]);
-  const [searchValue , setSearchValue ] = useState('');
+import AddFavourites from './components/AddFavourites';
+import RemoveFavourites from './components/RemoveFavourites';
+
+const App = () => {
+	const [movies, setMovies] = useState([]);
+	const [favourites, setFavourites] = useState([]);
+	const [searchValue, setSearchValue] = useState('');
+
+	const getMovieRequest = async (searchValue) => {
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=950dc40f`;
+
+		const response = await fetch(url);
+		const responseJson = await response.json();
+
+		if (responseJson.Search) {
+			setMovies(responseJson.Search);
+		}
+	};
 
 
-  const getMoviesRequest = async (searchValue) =>{
-    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=950dc40f`;
+	useEffect(() => {
+		getMovieRequest(searchValue);
+	}, [searchValue]);
 
-    const responce  = await fetch(url);
-    const responseJson = await responce.json();
+	useEffect(() => {
+		const movieFavourites = JSON.parse(
+			localStorage.getItem('react-movie-app-favourites')
+		);
 
+		if (movieFavourites) {
+			setFavourites(movieFavourites);
+		}
+	}, []);
 
-    if(responseJson.Search){
-      setMovies(responseJson.Search);
-    }
-  };
+	const saveToLocalStorage = (items) => {
+		localStorage.setItem('react-movie-app-favourites', JSON.stringify(items));
+	};
 
+	const addFavouriteMovie = (movie) => {
+		const newFavouriteList = [...favourites, movie];
+		setFavourites(newFavouriteList);
+		saveToLocalStorage(newFavouriteList);
+	};
 
-useEffect(() => {
-   getMoviesRequest(searchValue);
-}, [searchValue])
+	const removeFavouriteMovie = (movie) => {
+		const newFavouriteList = favourites.filter(
+			(favourite) => favourite.imdbID !== movie.imdbID
+		);
 
-  return (
-    <div className="container-fluid movie-app">
-      <div className="row d-flex align-items-center mt-4 mb-4">
-        <MovieListHeading heading ="Movies"  />
-        <SearchBox searchValue = {searchValue} setSearchValue= {setSearchValue} />
-      </div>
+		setFavourites(newFavouriteList);
+		saveToLocalStorage(newFavouriteList);
+	};
 
-    <MovieList
-    movies ={movies}
-    />
-    </div>
-  );
-}
+	return (
+		<div className='container-fluid movie-app'>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Movies' />
+				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+			</div>
+			<div className='row'>
+				<MovieList
+					movies={movies}
+					handleFavouritesClick={addFavouriteMovie}
+					favouriteComponent={AddFavourites}
+				/>
+			</div>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Favourites' />
+			</div>
+			<div className='row'>
+				<MovieList
+					movies={favourites}
+					handleFavouritesClick={removeFavouriteMovie}
+					favouriteComponent={RemoveFavourites}
+				/>
+			</div>
+		</div>
+	);
+};
 
 export default App;
--- project-2/src/components/MovieList.jsx
+++ project-2/src/components/MovieList.jsx
@@ -1,17 +1,27 @@
-const MovieList = (props) => {
-    return (
-        <>
-         {props.movies.map((
-            movie , index
-         ) => 
-         <div className="image-container d-flex justify-content-start m3">
+import React from 'react';
 
-            <img src={movie.Poster} alt="movie" srcset="" />
-         </div>
-         
-         )}
-        </>
-    )
-} 
+const MovieList = (props) => {
+  const { movies, handleFavouritesClick, favouriteComponent } = props;
+
+  return (
+    <>
+      {movies.map((movie, index) => (
+        <div key={index} className='image-container d-flex justify-content-center m-2'>
+          <img
+            src={movie.Poster}
+            alt='movie'
+          
+          />
+          <div
+            onClick={() => handleFavouritesClick(movie)}
+            className='overlay d-flex align-items-center justify-content-center'
+          >
+            {React.createElement(favouriteComponent)}
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
 
 export default MovieList;
