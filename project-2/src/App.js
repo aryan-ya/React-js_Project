d2dbe21359495ab35e36eefd46cd9e0aff7e57d7
Aryan Kumar Yadav <ay3978943@gmail.com>
2023-12-29 10:58:21 +0530

    project2

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -1,34 +1,40 @@
 
-import './App.css';
+
 import React , {useState , useEffect} from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MovieList from './components/MovieList';
-
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
+import "./App.css";
 function App() {
-  const [movies , setMovies] = useState([    {
-    "Title": "Star Wars: Episode IV - A New Hope",
-    "Year": "1977",
-    "imdbID": "tt0076759",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg"
-},
-{
-    "Title": "Star Wars: Episode V - The Empire Strikes Back",
-    "Year": "1980",
-    "imdbID": "tt0080684",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-},
-{
-    "Title": "Star Wars: Episode VI - Return of the Jedi",
-    "Year": "1983",
-    "imdbID": "tt0086190",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
-},]);
+  const [movies , setMovies] = useState([ ]);
+  const [searchValue , setSearchValue ] = useState('');
+
+
+  const getMoviesRequest = async (searchValue) =>{
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=950dc40f`;
+
+    const responce  = await fetch(url);
+    const responseJson = await responce.json();
+
+
+    if(responseJson.Search){
+      setMovies(responseJson.Search);
+    }
+  };
+
+
+useEffect(() => {
+   getMoviesRequest(searchValue);
+}, [searchValue])
+
   return (
     <div className="container-fluid movie-app">
-      <div className="row"></div>
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading ="Movies"  />
+        <SearchBox searchValue = {searchValue} setSearchValue= {setSearchValue} />
+      </div>
+
     <MovieList
     movies ={movies}
     />
