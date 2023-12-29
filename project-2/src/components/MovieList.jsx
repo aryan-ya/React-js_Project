44088772ef65d7febe86c96414da2da0313fb068
Aryan Kumar Yadav <ay3978943@gmail.com>
2023-12-29 07:37:59 +0530

    project2

---
+++ project-2/src/components/MovieList.jsx
@@ -0,0 +1,17 @@
+const MovieList = (props) => {
+    return (
+        <>
+         {props.movies.map((
+            movie , index
+         ) => 
+         <div className="image-container d-flex justify-content-start m3">
+
+            <img src={movie.Poster} alt="movie" srcset="" />
+         </div>
+         
+         )}
+        </>
+    )
+} 
+
+export default MovieList;
