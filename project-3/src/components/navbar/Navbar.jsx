9f131eb6657c0183481c919c5a82a1bb3b9186b6
Aryan Kumar Yadav <ay3978943@gmail.com>
2024-01-10 04:27:24 +0530

    nav

--- project-3/src/components/navbar/Navbar.jsx
+++ project-3/src/components/navbar/Navbar.jsx
@@ -3,7 +3,10 @@ import "./navbar.css";
 
 const Navbar = () => {
   return (
+    <>
     <div>Navbar Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos veniam asperiores porro fugit eaque repellat similique accusamus qui! Sunt sed sint aperiam similique doloremque aliquid mollitia commodi eligendi voluptatibus fuga.</div>
+
+    <ul> <li></li></ul></>
   )
 }
 
